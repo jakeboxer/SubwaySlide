bf1757aec85af58607e8fa941d22fdd49cb921ec
Jake Boxer <jacob.boxer@gmail.com>
2011-03-21 20:03:37 -0400

    Initial Commit

---
+++ SubwaySlide/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  SubwaySlide
+//
+//  Created by Jacob Boxer on 3/21/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ SubwaySlide/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SubwaySlide
+//
+//  Created by Jacob Boxer on 3/21/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
